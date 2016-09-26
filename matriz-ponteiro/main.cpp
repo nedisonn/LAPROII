8fdc9d2e03ab56c665e09b0e75dd93bb51d87fa0
NEDISON NETO FARIA VELLOSO <16104071@l1976580.portoalegre.pucrsnet.br>
2016-09-26 09:03:10 -0300

    o

---
+++ matriz-ponteiro/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main (){
+	int colunas, **matriz,
+	matriz = new int*;
+
+	ofstream arq;
+	arq.open("vetor.txt", ios:: out );
+		if (!arq.is_open()) return 0;
+	getline (arq, i);
+	int linha[i]
+	while (arq.good())
+		
+	 
+
+
+
+
+return 0;
+}
